c69c31670531417ee3eecbc24669fa4da5db155c
xxxelppa <xxxelppa@gmail.com>
2020-10-06 12:43:40 +0900

    level_2 :: removing pairs

--- src/com/nimkoes/algo/level_2/_024_removing_pairs.java
+++ src/com/nimkoes/algo/level_2/_024_removing_pairs.java
@@ -5,6 +5,8 @@ import java.util.Stack;
 public class _024_removing_pairs {
     
     public int solution(String s) {
+        if (s.length() % 2 != 0) return 0;
+        
         Stack<Character> st = new Stack<>();
         for (char c : s.toCharArray()) {
             if (st.empty() || st.peek() != c) st.push(c);
