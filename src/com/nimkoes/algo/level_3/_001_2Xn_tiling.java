6d6fa3633891ef6303e8dd714df9b84c6f9f721c
xxxelppa <xxxelppa@gmail.com>
2020-10-14 13:30:57 +0900

    level_3 :: 2 x n tiling add loop solution

--- src/com/nimkoes/algo/level_3/_001_2Xn_tiling.java
+++ src/com/nimkoes/algo/level_3/_001_2Xn_tiling.java
@@ -4,7 +4,23 @@ public class _001_2Xn_tiling {
     int[] memory;
     public int solution(int n) {
         memory = new int[n];
-        return calc(n);
+    
+        /*
+         * recursive solution
+         */
+        // return calc(n);
+        
+        
+        /*
+         * loop solution
+         */
+        if(n < 4) return n;
+        
+        memory[0] = 1;
+        memory[1] = 2;
+        for(int i = 2; i < n; ++i) memory[i] = (memory[i-2] + memory[i-1]) % 1000000007;
+        
+        return memory[n-1];
     }
     
     private int calc(int n) {
