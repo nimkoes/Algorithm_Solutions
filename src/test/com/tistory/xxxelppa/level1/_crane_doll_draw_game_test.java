3e7b2560e6d0f84d6ec5b82d16feab65c5d9c944
nimkoes <xxxelppa@gmail.com>
2021-04-28 20:25:32 +0900

    레거시 코드 삭제, 문제 풀이 등록, 테스트 코드 추가, README.md 수정

---
+++ src/test/com/tistory/xxxelppa/level1/_crane_doll_draw_game_test.java
@@ -0,0 +1,27 @@
+package com.tistory.xxxelppa.level1;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _crane_doll_draw_game_test {
+    
+    _crane_doll_draw_game solution;
+    
+    @BeforeEach
+    void setUp() {
+        solution = new _crane_doll_draw_game();
+    }
+    
+    @Test
+    @DisplayName("테스트 케이스 1")
+    void solution() {
+        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
+        int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
+    
+        assertThat(solution.solution(board, moves)).isEqualTo(4);
+    }
+}
