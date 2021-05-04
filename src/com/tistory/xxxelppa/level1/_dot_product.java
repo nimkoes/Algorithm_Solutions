b942e56aa1dcbb9bfbfa1545e28d813f1a22f989
nimkoes <xxxelppa@gmail.com>
2021-05-04 22:26:40 +0900

    문제 풀이 추가 : 내적

---
+++ src/com/tistory/xxxelppa/level1/_dot_product.java
@@ -0,0 +1,13 @@
+package com.tistory.xxxelppa.level1;
+
+public class _dot_product {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        int len = a.length;
+        
+        for (int i = 0; i < len; ++i) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+}
