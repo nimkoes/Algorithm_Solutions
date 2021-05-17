da383335ff54997dd3b22df3764c80eeccff7d79
nimkoes <xxxelppa@gmail.com>
2021-05-18 08:28:59 +0900

    level2, 테스트 코드 수정

--- src/test/com/tistory/xxxelppa/level2/_make_the_minimum_test.java
+++ src/test/com/tistory/xxxelppa/level2/_make_the_minimum_test.java
@@ -6,53 +6,50 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("최솟값 만들기")
 class _make_the_minimum_test {
-    
+
     @Builder
     @Getter
     static class TestCase {
         int[] A;
         int[] B;
-        
+
         int expected;
     }
-    
+
     _make_the_minimum solution;
-    ArrayList<TestCase> testCase;
-    
+
     @BeforeEach
     void setUp() {
         solution = new _make_the_minimum();
-        testCase = new ArrayList<>();
-        
-        testCase.add(
-                new TestCase.TestCaseBuilder()
-                        .A(new int[]{1, 4, 2})
-                        .B(new int[]{5, 4, 4})
-                        .expected(29)
-                        .build()
-        );
-        
-        testCase.add(
-                new TestCase.TestCaseBuilder()
-                        .A(new int[]{1, 2})
-                        .B(new int[]{3, 4})
-                        .expected(10)
-                        .build()
-        );
     }
-    
+
     @Test
-    @DisplayName("입출력 예 테스트")
-    void name() {
-        for (TestCase aCase : testCase) {
-            assertThat(solution.solution(aCase.getA(), aCase.getB()))
-                    .isEqualTo(aCase.getExpected());
-        }
+    @DisplayName("입출력 예 테스트 1")
+    void test_01() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .A(new int[]{1, 4, 2})
+                .B(new int[]{5, 4, 4})
+                .expected(29)
+                .build();
+
+        assertThat(solution.solution(testCase.getA(), testCase.getB()))
+                .isEqualTo(testCase.getExpected());
+    }
+
+    @Test
+    @DisplayName("입출력 예 테스트 2")
+    void test_02() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .A(new int[]{1, 2})
+                .B(new int[]{3, 4})
+                .expected(10)
+                .build();
+
+        assertThat(solution.solution(testCase.getA(), testCase.getB()))
+                .isEqualTo(testCase.getExpected());
     }
 }
--- src/test/com/tistory/xxxelppa/level2/_matching_and_removing_test.java
+++ src/test/com/tistory/xxxelppa/level2/_matching_and_removing_test.java
@@ -6,51 +6,47 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("짝지어 제거하기")
 class _matching_and_removing_test {
-    
+
     @Builder
     @Getter
     static class TestCase {
         String s;
-        
+
         int expected;
     }
-    
+
     _matching_and_removing solution;
-    ArrayList<TestCase> testCase;
-    
+
     @BeforeEach
     void setUp() {
         solution = new _matching_and_removing();
-        testCase = new ArrayList<>();
-        
-        testCase.add(
-                new TestCase.TestCaseBuilder()
-                        .s("baabaa")
-                        .expected(1)
-                        .build()
-        );
-        
-        testCase.add(
-                new TestCase.TestCaseBuilder()
-                        .s("cdcd")
-                        .expected(0)
-                        .build()
-        );
-        
     }
-    
+
     @Test
-    @DisplayName("입출력 예 테스트")
-    void solution() {
-        for (TestCase aCase : testCase) {
-            assertThat(solution.solution(aCase.getS()))
-                    .isEqualTo(aCase.getExpected());
-        }
+    @DisplayName("입출력 예 테스트 1")
+    void test_01() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .s("baabaa")
+                .expected(1)
+                .build();
+
+        assertThat(solution.solution(testCase.getS()))
+                .isEqualTo(testCase.getExpected());
+    }
+
+    @Test
+    @DisplayName("입출력 예 테스트 2")
+    void test_02() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .s("cdcd")
+                .expected(0)
+                .build();
+
+        assertThat(solution.solution(testCase.getS()))
+                .isEqualTo(testCase.getExpected());
     }
 }
