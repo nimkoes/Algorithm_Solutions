d42e18f87c9ed541a081abcd715698e7c7eef383
nimkoes <xxxelppa@gmail.com>
2021-05-22 16:29:25 +0900

    level2, 테스트 코드 수정

--- src/test/com/tistory/xxxelppa/level2/_number_of_124_countries_test.java
+++ src/test/com/tistory/xxxelppa/level2/_number_of_124_countries_test.java
@@ -6,64 +6,71 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("124 나라의 숫자")
 class _number_of_124_countries_test {
-    
+
     @Builder
     @Getter
     static class TestCase {
         int n;
-        
+
         String expected;
     }
-    
+
     _number_of_124_countries solution;
-    ArrayList<TestCase> testCase;
-    
+
     @BeforeEach
     void setUp() {
         solution = new _number_of_124_countries();
-        testCase = new ArrayList<>();
-        
-        testCase.add(
-                new TestCase.TestCaseBuilder()
-                        .n(1)
-                        .expected("1")
-                        .build()
-        );
-        
-        testCase.add(
-                new TestCase.TestCaseBuilder()
-                        .n(2)
-                        .expected("2")
-                        .build()
-        );
-        
-        testCase.add(
-                new TestCase.TestCaseBuilder()
-                        .n(3)
-                        .expected("4")
-                        .build()
-        );
-        
-        testCase.add(
-                new TestCase.TestCaseBuilder()
-                        .n(4)
-                        .expected("11")
-                        .build()
-        );
     }
-    
+
     @Test
-    @DisplayName("입출력 예 테스트")
+    @DisplayName("입출력 예 테스트 1")
     void solution() {
-        for (TestCase aCase : testCase) {
-            assertThat(solution.solution(aCase.n))
-                    .isEqualTo(aCase.getExpected());
-        }
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .n(1)
+                .expected("1")
+                .build();
+
+        assertThat(solution.solution(testCase.n))
+                .isEqualTo(testCase.getExpected());
+    }
+
+    @Test
+    @DisplayName("입출력 예 테스트 2")
+    void test_01() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .n(2)
+                .expected("2")
+                .build();
+
+        assertThat(solution.solution(testCase.n))
+                .isEqualTo(testCase.getExpected());
+    }
+
+    @Test
+    @DisplayName("입출력 예 테스트 3")
+    void test_02() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .n(3)
+                .expected("4")
+                .build();
+
+        assertThat(solution.solution(testCase.n))
+                .isEqualTo(testCase.getExpected());
+    }
+
+    @Test
+    @DisplayName("입출력 예 테스트 4")
+    void test_03() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .n(4)
+                .expected("11")
+                .build();
+
+        assertThat(solution.solution(testCase.n))
+                .isEqualTo(testCase.getExpected());
     }
 }
--- src/test/com/tistory/xxxelppa/level2/_printer_test.java
+++ src/test/com/tistory/xxxelppa/level2/_printer_test.java
@@ -6,53 +6,50 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("프린터")
 class _printer_test {
-    
+
     @Builder
     @Getter
     static class TestCase {
         int[] priorities;
         int location;
-        
+
         int expected;
     }
-    
+
     _printer solution;
-    ArrayList<TestCase> testCase;
-    
+
     @BeforeEach
     void setUp() {
         solution = new _printer();
-        testCase = new ArrayList<>();
-        
-        testCase.add(
-                new TestCase.TestCaseBuilder()
-                        .priorities(new int[]{2, 1, 3, 2})
-                        .location(2)
-                        .expected(1)
-                        .build()
-        );
-        
-        testCase.add(
-                new TestCase.TestCaseBuilder()
-                        .priorities(new int[]{1, 1, 9, 1, 1, 1})
-                        .location(0)
-                        .expected(5)
-                        .build()
-        );
     }
-    
+
     @Test
-    @DisplayName("입출력 예 테스트")
-    void name() {
-        for (TestCase aCase : testCase) {
-            assertThat(solution.solution(aCase.getPriorities(), aCase.getLocation()))
-                    .isEqualTo(aCase.getExpected());
-        }
+    @DisplayName("입출력 예 테스트 1")
+    void test_01() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .priorities(new int[]{2, 1, 3, 2})
+                .location(2)
+                .expected(1)
+                .build();
+
+        assertThat(solution.solution(testCase.getPriorities(), testCase.getLocation()))
+                .isEqualTo(testCase.getExpected());
+    }
+
+    @Test
+    @DisplayName("입출력 예 테스트 2")
+    void test_02() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .priorities(new int[]{1, 1, 9, 1, 1, 1})
+                .location(0)
+                .expected(5)
+                .build();
+
+        assertThat(solution.solution(testCase.getPriorities(), testCase.getLocation()))
+                .isEqualTo(testCase.getExpected());
     }
 }
