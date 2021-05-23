4e79511a67f0d0abdbd48d83763521b04fb6865c
nimkoes <xxxelppa@gmail.com>
2021-05-24 08:26:09 +0900

    level2, 테스트 코드 수정

--- src/test/com/tistory/xxxelppa/level2/_skill_tree_test.java
+++ src/test/com/tistory/xxxelppa/level2/_skill_tree_test.java
@@ -6,45 +6,37 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("스킬트리")
 class _skill_tree_test {
-    
+
     @Builder
     @Getter
     static class TestCase {
         String skill;
         String[] skill_trees;
-        
+
         int expected;
     }
-    
+
     _skill_tree solution;
-    ArrayList<TestCase> testCase;
-    
+
     @BeforeEach
     void setUp() {
         solution = new _skill_tree();
-        testCase = new ArrayList<>();
-        
-        testCase.add(
-                new TestCase.TestCaseBuilder()
-                        .skill("CBD")
-                        .skill_trees(new String[]{"BACDE", "CBADF", "AECB", "BDA"})
-                        .expected(2)
-                        .build()
-        );
     }
-    
+
     @Test
-    @DisplayName("입출력 예 테스트")
-    void name() {
-        for (TestCase aCase : testCase) {
-            assertThat(solution.solution(aCase.getSkill(), aCase.getSkill_trees()))
-                    .isEqualTo(aCase.getExpected());
-        }
+    @DisplayName("입출력 예 테스트 1")
+    void test_01() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .skill("CBD")
+                .skill_trees(new String[]{"BACDE", "CBADF", "AECB", "BDA"})
+                .expected(2)
+                .build();
+
+        assertThat(solution.solution(testCase.getSkill(), testCase.getSkill_trees()))
+                .isEqualTo(testCase.getExpected());
     }
 }
