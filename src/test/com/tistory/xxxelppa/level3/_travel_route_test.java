363f26be8ee9068cf02ebf6f31c6c3efdf580825
nimkoes <xxxelppa@gmail.com>
2021-08-02 22:44:11 +0900

    mod test code

--- src/test/com/tistory/xxxelppa/level3/_travel_route_test.java
+++ src/test/com/tistory/xxxelppa/level3/_travel_route_test.java
@@ -6,10 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("여행경로")
 class _travel_route_test {
 
     @Builder
@@ -21,35 +20,33 @@ class _travel_route_test {
     }
 
     _travel_route solution;
-    ArrayList<TestCase> testCase;
-
 
     @BeforeEach
     void setUp() {
         solution = new _travel_route();
-        testCase = new ArrayList<>();
-
-        testCase.add(
-                new TestCase.TestCaseBuilder()
-                        .tickets(new String[][]{{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}})
-                        .expected(new String[]{"ICN", "JFK", "HND", "IAD"})
-                        .build()
-        );
-
-        testCase.add(
-                new TestCase.TestCaseBuilder()
-                        .tickets(new String[][]{{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL", "SFO"}})
-                        .expected(new String[]{"ICN", "ATL", "ICN", "SFO", "ATL", "SFO"})
-                        .build()
-        );
     }
 
     @Test
-    @DisplayName("입출력 예 테스트")
-    void name() {
-        for (TestCase aCase : testCase) {
-            assertThat(solution.solution(aCase.getTickets()))
-                    .isEqualTo(aCase.getExpected());
-        }
+    @DisplayName("입출력 예 테스트 1")
+    void test_01() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .tickets(new String[][]{{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}})
+                .expected(new String[]{"ICN", "JFK", "HND", "IAD"})
+                .build();
+
+        assertThat(solution.solution(testCase.getTickets()))
+                .isEqualTo(testCase.getExpected());
+    }
+
+    @Test
+    @DisplayName("입출력 예 테스트 2")
+    void test_02() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .tickets(new String[][]{{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL", "SFO"}})
+                .expected(new String[]{"ICN", "ATL", "ICN", "SFO", "ATL", "SFO"})
+                .build();
+
+        assertThat(solution.solution(testCase.getTickets()))
+                .isEqualTo(testCase.getExpected());
     }
 }
