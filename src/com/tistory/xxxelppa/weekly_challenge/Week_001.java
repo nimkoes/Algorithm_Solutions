9b567108df05acf1ae97ddcabf98fd0512c807a5
nimkoes <xxxelppa@gmail.com>
2021-08-08 21:04:04 +0900

    add test code, week 001

--- src/com/tistory/xxxelppa/weekly_challenge/Week_001.java
+++ src/com/tistory/xxxelppa/weekly_challenge/Week_001.java
@@ -1,6 +1,6 @@
 package com.tistory.xxxelppa.weekly_challenge;
 
-public class week_001 {
+public class Week_001 {
     public long solution(int price, int money, int count) {
         long p = price, m = money, c = count;
         return Math.max(((p + (p * c)) * c / 2) - m, 0);
