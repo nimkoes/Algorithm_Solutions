48a7b31c4fbdbb317cd0b48f74121bba9ca43d84
xxxelppa <xxxelppa@gmail.com>
2022-02-25 16:47:40 +0900

    문제 풀이 준비

---
+++ src/com/tistory/xxxelppa/level1/_get_report_results.java
@@ -0,0 +1,8 @@
+package com.tistory.xxxelppa.level1;
+
+public class _get_report_results {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        return answer;
+    }
+}
