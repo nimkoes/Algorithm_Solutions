8518b0ae7600105272169353eca7034c516b4d16
nimkoes <xxxelppa@gmail.com>
2022-11-05 00:02:39 +0900

    옹알이(1) 문제 풀이

--- src/com/tistory/xxxelppa/level0/_babbling_1.java
+++ src/com/tistory/xxxelppa/level0/_babbling_1.java
@@ -4,9 +4,7 @@ public class _babbling_1 {
     public int solution(String[] babbling) {
         int answer = 0;
         for (String b : babbling) {
-            if (b.matches("(aya|ye|woo|ma)+")) {
-                ++answer;
-            }
+            if (b.matches("(aya|ye|woo|ma)+")) ++answer;
         }
         return answer;
     }
