5c93ef742a3dff96b6c572269e55e70b914d4331
nimkoes <xxxelppa@gmail.com>
2022-11-16 00:21:45 +0900

    문제 풀이 추가

---
+++ src/com/nimkoes/level0/Solution_097.java
@@ -0,0 +1,11 @@
+package com.nimkoes.level0;
+
+public class Solution_097 {
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        int n = num * (num + 1) / 2;
+        int start = (total - n) / num;
+        for (int i = 1; i <= num; ++i) answer[i - 1] = start + i;
+        return answer;
+    }
+}
---
+++ src/test/com/nimkoes/level0/Solution_097_test.java
@@ -0,0 +1,81 @@
+package com.nimkoes.level0;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("연속된 수의 합")
+class Solution_097_test {
+
+    @Builder
+    @Getter
+    static class TestCase {
+        int num;
+        int total;
+        int[] expected;
+    }
+
+    Solution_097 solution;
+
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution_097();
+    }
+
+    @Test
+    @DisplayName("입출력 예 테스트 1")
+    void test_01() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .num(3)
+                .total(12)
+                .expected(new int[]{3, 4, 5})
+                .build();
+
+        assertThat(solution.solution(testCase.getNum(), testCase.getTotal()))
+                .isEqualTo(testCase.getExpected());
+    }
+
+    @Test
+    @DisplayName("입출력 예 테스트 2")
+    void test_02() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .num(5)
+                .total(15)
+                .expected(new int[]{1, 2, 3, 4, 5})
+                .build();
+
+        assertThat(solution.solution(testCase.getNum(), testCase.getTotal()))
+                .isEqualTo(testCase.getExpected());
+    }
+
+    @Test
+    @DisplayName("입출력 예 테스트 3")
+    void test_03() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .num(4)
+                .total(14)
+                .expected(new int[]{2, 3, 4, 5})
+                .build();
+
+        assertThat(solution.solution(testCase.getNum(), testCase.getTotal()))
+                .isEqualTo(testCase.getExpected());
+    }
+
+    @Test
+    @DisplayName("입출력 예 테스트 4")
+    void test_04() {
+        TestCase testCase = new TestCase.TestCaseBuilder()
+                .num(5)
+                .total(5)
+                .expected(new int[]{-1, 0, 1, 2, 3})
+                .build();
+
+        assertThat(solution.solution(testCase.getNum(), testCase.getTotal()))
+                .isEqualTo(testCase.getExpected());
+    }
+}
