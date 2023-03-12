5ed57f2a74854cdf0cb92652b0e264a9485bf80e
nimkoes <xxxelppa@gmail.com>
2023-03-12 14:44:40 +0900

    문제 풀이 추가

--- src/com/nimkoes/leetcode/medium/Solution_010.java
+++ src/com/nimkoes/leetcode/medium/Solution_010.java
@@ -1,13 +1,21 @@
 package com.nimkoes.leetcode.medium;
 
+import java.util.Arrays;
+
 public class Solution_010 {
     public int maxCoins(int[] piles) {
-        return 0;
+        Arrays.parallelSort(piles);
+
+        int result = 0, size = piles.length;
+        int[] newPiles = Arrays.copyOfRange(piles, size / 3, size);
+        for (int i = 0; i < newPiles.length; i += 2) result += newPiles[i];
+
+        return result;
     }
 }
 
 /* result summary
-Runtime: ms, faster than % of Java online submissions for Maximum Number of Coins You Can Get.
-Memory Usage:  MB, less than % of Java online submissions for Maximum Number of Coins You Can Get.
+Runtime: 32 ms, faster than 58.44% of Java online submissions for Maximum Number of Coins You Can Get.
+Memory Usage:  51.5 MB, less than 34.80% of Java online submissions for Maximum Number of Coins You Can Get.
  */
 
