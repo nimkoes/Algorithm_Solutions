70d8d271e875956412af01c44fcf61b844f5266e
nimkoes <xxxelppa@gmail.com>
2023-04-16 22:09:26 +0900

    문제 풀이 준비

--- src/com/nimkoes/leetcode/medium/Solution_015.java
+++ src/com/nimkoes/leetcode/medium/Solution_015.java
@@ -2,13 +2,19 @@ package com.nimkoes.leetcode.medium;
 
 public class Solution_015 {
     public int countTriplets(int[] arr) {
-
-        return 0;
+        int result = 0;
+        for (int i = 0; i < arr.length; ++i) {
+            int temp = arr[i];
+            for (int j = i + 1; j < arr.length; ++j) {
+                if ((temp ^= arr[j]) == 0) result += (j - i);
+            }
+        }
+        return result;
     }
 }
 
 /* result summary
-Runtime: ms, faster than % of Count Triplets That Can Form Two Arrays of Equal XOR.
-Memory Usage: MB, less than % of Count Triplets That Can Form Two Arrays of Equal XOR.
+Runtime: 1ms, faster than 100% of Count Triplets That Can Form Two Arrays of Equal XOR.
+Memory Usage: 40.2MB, less than 67.86% of Count Triplets That Can Form Two Arrays of Equal XOR.
  */
 
